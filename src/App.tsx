cfedf5c3264534a8f3261bedd9b4c0dd411dace9
AbdoAKAGames <ma2008abdoooo@gmail.com>
2024-11-11 18:13:15 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -464,7 +464,7 @@ useEffect(() => {
       localStorage.setItem("current_part", JSON.stringify([{ part_num: null, scroll_top: null }]))
     } else {
       if (JSON.parse(localStorage.current_part)[0].part_num != null) {
-        setCurrentSurahNass(JSON.parse(localStorage.current_part)[0].part_num);
+        setCurrentPartNass(JSON.parse(localStorage.current_part)[0].part_num);
         let int = setInterval(() => {
           let el = document.getElementsByClassName('quran-part-nass')[0] as HTMLDivElement;
           el.scrollTo({ top: JSON.parse(localStorage.current_part)[0].scroll_top, behavior: 'smooth' });
