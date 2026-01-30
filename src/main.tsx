b95a9bcc7289157fd1733de94b42e555769f4ea1
AbdoAKAGames <ma2008abdoooo@gmail.com>
2026-01-30 22:07:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import PhoneHome from './components/phone-home/PhoneHome.tsx'
+
+const phoneRegEx = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
+const userAgent = navigator.userAgent;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {phoneRegEx.test(userAgent) ? <PhoneHome /> : <App />}
   </StrictMode>,
 )
